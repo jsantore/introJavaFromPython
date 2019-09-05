3f1cb0378ebab95ffd9db038f4d111c9b8f4bea7
johns <jsantore@bridgew.edu>
2019-09-05 15:34:56 -0400

    reading from file into list and printing to screen working
    Signed-off-by: johns <jsantore@bridgew.edu>

--- src/FileExample.java
+++ src/FileExample.java
@@ -1,7 +1,16 @@
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class FileExample {
     public static void main (String[] args)throws IOException{
-        
+        //part 1 reading into the list
+        var fileName = "students.txt";
+        String all_names = Files.readString(Paths.get(fileName));
+        String[] name_list = all_names.split("\n");
+        //part2 - printing the list
+        for (String name: name_list){
+            System.out.println(name);
+        }
     }
 }
